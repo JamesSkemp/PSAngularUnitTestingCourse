3e02bbb4dfc94db7b28cf961753390497661f12c
James Skemp <strivinglife@gmail.com>
2021-08-09 14:35:46 -0500

    Add shallow heroes component template tests

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,4 +1,10 @@
+import { Component, Input, NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { RouterTestingModule } from "@angular/router/testing";
 import { of } from "rxjs";
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component";
 
 describe('HeroesComponent', () => {
@@ -52,4 +58,80 @@ describe('HeroesComponent', () => {
         });
     });
 
+});
+
+describe('HeroesComponent (shallow)', () => {
+    let fixture: ComponentFixture<HeroesComponent>;
+    let mockHeroService;
+    let HEROES;
+
+    beforeEach(() => {
+        HEROES = [
+            {id: 1, name: 'SpiderDude', strength: 8},
+            {id: 2, name: 'Woderful Woman', strength: 24},
+            {id: 3, name: 'SuperDude', strength: 55}
+        ];
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+        TestBed.configureTestingModule({
+            declarations: [HeroesComponent],
+            imports: [RouterTestingModule],
+            providers: [{ provide: HeroService, useValue: mockHeroService }],
+            // ignore template errors
+            schemas: [NO_ERRORS_SCHEMA]
+        });
+        fixture = TestBed.createComponent(HeroesComponent);
+    });
+
+    it('should set heroes correctly from the service', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+        fixture.detectChanges();
+
+        expect(fixture.componentInstance.heroes.length).toBe(3);
+    });
+});
+
+describe('HeroesComponent (shallow w/ no schema)', () => {
+    let fixture: ComponentFixture<HeroesComponent>;
+    let mockHeroService;
+    let HEROES;
+
+    @Component({
+        selector: 'app-hero',
+        template: '<div></div>'
+    })
+    class FakeHeroComponent {
+        @Input() hero: Hero;
+        //@Output() delete = new EventEmitter();
+    }
+
+    beforeEach(() => {
+        HEROES = [
+            {id: 1, name: 'SpiderDude', strength: 8},
+            {id: 2, name: 'Woderful Woman', strength: 24},
+            {id: 3, name: 'SuperDude', strength: 55}
+        ];
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+        TestBed.configureTestingModule({
+            declarations: [HeroesComponent, FakeHeroComponent],
+            imports: [RouterTestingModule],
+            providers: [{ provide: HeroService, useValue: mockHeroService }]
+        });
+        fixture = TestBed.createComponent(HeroesComponent);
+    });
+
+    it('should set heroes correctly from the service', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+        fixture.detectChanges();
+
+        expect(fixture.componentInstance.heroes.length).toBe(3);
+    });
+
+    it('should create one li for each hero', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+        fixture.detectChanges();
+
+        expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+    });
 });
